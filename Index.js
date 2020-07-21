70c12d87e284b9410c61c0da3af22c18e70b78f3
bogyung99 <bogung1208@naver.com>
2020-07-21 17:09:24 +0900

    let

--- Index.js
+++ Index.js
@@ -1,1 +1,7 @@
-alert('Im Working. Im JS. Im beautiful. Im worth it');
+// alert('Im Working. Im JS. Im beautiful. Im worth it');
+// console.log('Im Working. Im JS. Im beautiful. Im worth it');
+
+let a = 221
+let b = a - 5
+
+console.log(b);
